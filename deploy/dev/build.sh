9753500d2e1d2dfaf4c224615cf0cde11d7a85a4
DinhLamTruong <dinhlamtruong1998@gmail.com>
2025-09-09 10:18:09 +0700

    add source code

---
+++ deploy/dev/build.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+docker --version
+docker build -f Dockerfile -t ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} . --network host
+docker image ls
+echo "...[done] build image "$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG""
+
